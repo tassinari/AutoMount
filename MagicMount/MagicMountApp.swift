8caf2a39d6f969c282dbdc3ab10d30de6a833f35
Mark Tassinari <tassinari@gmail.com>
2025-12-24 13:09:21 -0500

    Initial Commit

---
+++ MagicMount/MagicMountApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicMountApp.swift
+//  MagicMount
+//
+//  Created by Mark Tassinari on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MagicMountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
